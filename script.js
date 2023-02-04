922f53e226ffb0ddd6c68d59d223382fd75a4e0d
Kagheni <Kaghenimbale@gmail.com>
2023-02-04 12:36:49 +0200

    Test linters on the project

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const aside = document.querySelector('.aside');
 const dot = document.querySelector('.btn-dot');
-const container = document.querySelector('.container')
-const out =document.querySelector('.out i');
-const menu=document.querySelector('.fa-bars');
-const navlink=document.querySelector('.nav-link');
+const container = document.querySelector('.container');
+const out = document.querySelector('.out i');
+const menu = document.querySelector('.fa-bars');
+const navlink = document.querySelector('.nav-link');
 
-dot.addEventListener('click', ()=>{
-    aside.style.display="initial";
-    container.style.left="29rem";
-    console.log('clicked');
+dot.addEventListener('click', () => {
+  aside.style.display = 'initial';
+  container.style.left = '29rem';
 });
 
-out.addEventListener('click', ()=>{
-    aside.style.display="none";
-    container.style.left="10rem";
+out.addEventListener('click', () => {
+  aside.style.display = 'none';
+  container.style.left = '10rem';
 });
 
-menu.addEventListener('click', ()=>{
-    navlink.classList.toggle('mobile-nav');
+menu.addEventListener('click', () => {
+  navlink.classList.toggle('mobile-nav');
 });
 
 const links = document.querySelectorAll('.link');
 
-links.forEach((link)=>{
-    function renderClick () {
-        navlink.classList.toggle('mobile-nav');
-    };
-    link.addEventListener('click', renderClick);
+links.forEach((link) => {
+  function renderClick() {
+    navlink.classList.toggle('mobile-nav');
+  }
+  link.addEventListener('click', renderClick);
 });
